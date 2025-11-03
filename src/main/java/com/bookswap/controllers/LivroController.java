0e53087232eaa5f439ae8c146c7b1583cc3ed682
julia708 <antunesjuliamaria786@gmail.com>
2025-11-03 14:05:58 -0300

    fiz o repository. esperando o dao pra fazer a controller.

---
+++ src/main/java/com/bookswap/controllers/LivroController.java
@@ -0,0 +1,5 @@
+package com.bookswap.controllers;
+
+public class LivroController {
+    
+}
---
+++ src/main/java/com/bookswap/repository/LivroRepository.java
@@ -0,0 +1,38 @@
+package com.bookswap.repository; // ta comentado pra nao ficar aparecendo erro
+
+// import com.bookswap.dao.LivroDao;
+// import com.bookswap.models.Livro;
+
+// public class LivroRepository {
+    
+//     private final LivroDao livroDao;
+
+//     public LivroRepository() {
+//         this.livroDao = new LivroDao();
+//     }
+
+//     @Override
+//     public Livro findById(int id) {
+//         return livroDao.findById(id);
+//     }
+
+//     @Override
+//     public Livro findByTitulo(String email) {
+//         return livroDao.findByTitulo(email);
+//     }
+
+//     @Override
+//     public void save(Livro livro) {
+//         livroDao.save(Livro);
+//     }
+
+//     @Override
+//     public void update(Livro livro) {
+//         livroDao.update(Livro);
+//     }
+
+//     @Override
+//     public void delete(int id) {
+//         livroDao.delete(id);
+//     }
+// }
